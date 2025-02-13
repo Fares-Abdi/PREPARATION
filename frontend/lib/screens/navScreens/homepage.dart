c1d0026d058f9fcf1c47f4390016bd6513ee1393
TheTrueGrayWater <faresabdi2004@gmail.com>
2025-02-13 20:38:13 +0100

    Correction

--- frontend/lib/screens/navScreens/homepage.dart
+++ frontend/lib/screens/navScreens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../widgets/advanced_drawing_canvas.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,9 +11,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawing Board'),
+        elevation: 0,
+      ),
+      body: const SafeArea(
+        child: AdvancedDrawingCanvas(
+          initialColor: Colors.black,
+          initialStrokeWidth: 5.0,
+        ),
       ),
     );
   }
